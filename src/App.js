b145c587ed81962cf7054e09ed4bada0a5051562
WebWalterWrite <webwalterwhrite@gmail.com>
2019-10-01 17:20:58 +0200

    :art: add FakePost component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import FakePost from './components/fakers/FakePost';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo}  alt="logo" />
+        <h1>React-Share by</h1>
       </header>
+      <main>
+        <FakePost />
+      </main>
+      Photo par Kim Daniels sur Unsplash
     </div>
   );
 }
