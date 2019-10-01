c7689c382b954b8aa6f20d76c0a7d86e73a76e0f
WebWalterWrite <webwalterwhrite@gmail.com>
2019-10-01 17:22:44 +0200

    :beers: add fakepost data
    creation d'un objet de données factices pour le fakePost component

---
+++ src/components/fakers/fakedata.js
@@ -0,0 +1,8 @@
+export const data = {
+  title: "Tartelettes aux douceurs d'🍊ranges...",
+  author: 'Sammy',    
+  createdAt: '30/09/2019',
+  category: 'recettes',
+  url: 'http://172.20.10.2:3000/',
+  preview: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque similique veniam ea dicta illo commodi et nisi pariatur incidunt reiciendis laudantium, velit obcaecati quia mollitia aut dolorum quo magni sint...'
+};
