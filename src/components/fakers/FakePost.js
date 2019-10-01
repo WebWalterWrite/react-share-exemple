5330c0b00a9946f9f235b43528db6e88fc1eb242
WebWalterWrite <webwalterwhrite@gmail.com>
2019-10-01 17:24:49 +0200

    :beers: add fakePost component
    component d'exemple pour l'illustration du partage de contenu

---
+++ src/components/fakers/FakePost.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { FacebookShare, LinkedinShare, PinterestShare } from '../social-share/SocialShare';
+import { Category, Infos, Post, Title, Preview } from './fakepost.styled';
+import { Socials } from '../social-share/SocialContainer';
+import { data } from './fakedata';
+import sharing from '../../utils/style/kim-daniels-OrkEasJeY74-unsplash.jpg';
+
+const FakePost = () => {
+
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    setPost(data);
+  }, [post])
+
+  return (
+    <Post>
+      <img src={sharing} alt="" />
+      <Preview>
+        <Title itemProp="name">
+          {post.title}
+        </Title>
+        <Infos>
+          <Category>{post.category}</Category>
+          <p itemProp="director">
+            <span role="img" aria-label="pen">✍</span>
+            avec{" "}
+            <span role="img" aria-label="heart">❤️</span> par {post.author} le {post.createdAt}</p>
+          <p itemProp="description">{post.preview}</p>
+          <div className="_read-btn">lire la suite</div>
+
+
+          <Socials>
+            <LinkedinShare />
+            <FacebookShare />
+            <PinterestShare />
+          </Socials>
+
+        </Infos>
+      </Preview>
+    </Post>
+  )
+};
+
+export default FakePost;
