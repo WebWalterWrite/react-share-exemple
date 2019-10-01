4e1023e0bf82c08e4d3fa3ffd2fb873d1d6897c5
WebWalterWrite <webwalterwhrite@gmail.com>
2019-10-01 17:32:24 +0200

    type: data desc: add url de partage, relative au media social

---
+++ src/components/social-share/socialsUrl.js
@@ -0,0 +1,18 @@
+
+export const socialLink = {
+  linkedin: {
+    url: 'https://www.linkedin.com/sharing/share-offsite/?url=',
+    content: 'Linkedin'
+  },
+  facebook: {
+    url: 'https://www.facebook.com/sharer/sharer.php?u=',
+    content: 'Facebook'
+  },
+  pinterest:{
+    url:"https://pinterest.com/pin/create/bookmarklet/?media=[photo]&url=[url]&description=[description]"
+    ,
+    content: "pinterest"
+  }
+};
+
+export default socialLink;
